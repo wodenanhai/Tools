8897b0ec1f61e3d638bf13af6a5068a9901f8eab
zhangcheng <2778162192@qq.com>
2026-05-15 14:44:04 +0800

    commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QtQuickControls2/QQuickStyle>
+
+#include "PdfSplitService.h"
+
+int main(int argc, char *argv[])
+{
+    QQuickStyle::setStyle("Basic");
+
+    QGuiApplication app(argc, argv);
+    QCoreApplication::setApplicationName("PDF Studio Toolbox");
+    QGuiApplication::setApplicationDisplayName("PDF Studio Toolbox");
+
+    PdfSplitService pdfSplitService;
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("pdfSplitService", &pdfSplitService);
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("Tools", "Main");
+
+    return QGuiApplication::exec();
+}
